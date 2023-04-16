f9d3c367d72fd06288a3ee9914c7369759622ab0
samuel <samuelkelvinmwangi@gmail.com>
2023-04-16 18:34:43 +0300

    assingnment finishup

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const farmAnimals = ['cow', 'horse', 'sheep', 'pig', 'chicken'];
-
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 const muppet = {
@@ -46,3 +44,41 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+const farmAnimals = ['cow', 'horse', 'sheep', 'pig', 'chicken'];
+const [moo, neigh, baa, oink, cluck] = farmAnimals;
+const [bessie, , dolly, babe, little] = farmAnimals;
+const [blackAndWhite, , black, pink, ] = farmAnimals
+
+console.log (moo);
+console.log (neigh);
+console.log (baa);
+console.log (oink);
+console.log (cluck);
+
+console.log (bessie);
+console.log (dolly);
+console.log (babe);
+console.log (little);
+
+console.log (blackAndWhite);
+console.log (black);
+console.log (pink);
+
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = rainbowColors
+const [r, o, y, g, b, indg, v] = rainbowColors
+
+const muppets = {
+  muppetName: 'Miss Piggy',
+  color: 'pink',
+  song: 'Never Before, Never Again',
+  job: 'Cast member of The Muppet Show',
+  partner: 'Kermit',
+  song2: 'Moving Right Along',
+  song4: 'I Hope That Something Better Comes Along',
+  nestedJob: 'Host of The Muppet Show',
+  nestedPartner:'Miss Piggy'
+}
+
+const {muppetName, color, song, job, partner, song2, song4, nestedJob, nestedPartner} = muppets
